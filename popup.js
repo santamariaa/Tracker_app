314bd11604b94164eee1d8173206185279b06641
santamariaa <asantamaria101@gmail.com>
2016-03-17 18:08:15 -0500

    added icon, and fixed visits

--- popup.js
+++ popup.js
@@ -7,24 +7,43 @@ document.addEventListener('DOMContentLoaded', function() {
     activeTab = tab;
   });
   console.log(activeTab);
-  // var checkPageButton = document.getElementById('checkPage');
-  // checkPageButton.addEventListener('click', function() {
 
-  //   chrome.tabs.getSelected(null, function(tab) {
-  //     d = document;
+  var checkPageButton = document.getElementById('trackPage');
+  checkPageButton.addEventListener('click', function() {
 
-  //     var f = d.createElement('form');
-  //     f.action = 'http://gtmetrix.com/analyze.html?bm';
-  //     f.method = 'post';
-  //     var i = d.createElement('input');
-  //     i.type = 'hidden';
-  //     i.name = 'url';
-  //     i.value = tab.url;
-  //     f.appendChild(i);
-  //     d.body.appendChild(f);
-  //     f.submit();
-  //   });
-  // }, false);
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+      var form = d.createElement('form');
+      form.action = 'http://localhost:3000/api/v1/visits';
+      form.method = 'post';
+
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+
+      // var checkIn = d.createElement('input');
+      // checkIn.type = 'hidden';
+      // checkIn.name = 'checked_in';
+      // checkIn.value = true;
+
+      // var checkOut = d.createElement('input');
+      // checkOut.type = 'hidden';
+      // checkOut.name = 'checked_out';
+      // checkOut.value = true;
+
+      var email = document.getElementById("email").value;
+      var emailField = d.createElement('input');
+      emailField.type = 'hidden';
+      emailField.name = 'email';
+      emailField.value = email;
+
+      form.appendChild(i);
+      // form.appendChild(checkIn);
+      // form.appendChild(checkOut);
+      form.appendChild(emailField);
+      d.body.appendChild(form);
+      form.submit();
+    });
+  }, false);
 }, false);
-
-
